4e9c204f90748131746b822119456388efbcd627
rootRua <rootxbot@gmail.com>
2020-04-11 00:46:57 +0900

    20200410  정상적으로 작동하는 것을 확인 (기능 : 자동으로 페이지 들에 접속하는 것을 확인)

--- python/indeed.py
+++ python/indeed.py
@@ -2,7 +2,8 @@ import requests
 # 파이썬에서 요청을 모아둔 모듈  urllib보다 좋다.
 from bs4 import BeautifulSoup
 # 파서
-INDEED_URL = "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50"
+LIMIT = 50
+INDEED_URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
 
 def extract_indeed_pages():
     indeed_result = requests.get(INDEED_URL)
@@ -13,13 +14,20 @@ def extract_indeed_pages():
     #각 링크에서 페이지 함수를 가져오고 거기서 페이지 숫자만을 뜻하는 것을 다져온다.
     pagination = indeed_soup.find("div", {"class" : "pagination"})
     pages = pagination.find_all('a')
-    print(pages)
     spans = []
     for page in pages[0:-1]:
         spans.append(int(page.find("span").string))
     #spans = spans[0:-1]
 
     last_page = spans[-1]
+    return last_page
     #print(last_page) 마지막 숫자를 출력
     #print(spans)
     #print(range(last_page))  range함수는 ()안의 숫자만크 배열을 만들어준다
+
+
+def extract_indeed_jobs(last_page):
+    for page in range(last_page):
+        result = requests.get(f"{INDEED_URL}&start = {page * LIMIT}")
+        print(result.status_code)
+        #start_limit = []
